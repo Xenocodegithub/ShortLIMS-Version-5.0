aeab2e8e1a5ab6e2a19a821b8aed871afae93d84
Xenocodegithub <62418952+Xenocodegithub@users.noreply.github.com>
2021-03-23 18:49:24 +0530

    Add files via upload

---
+++ Serialization/Query.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serialization
+
+{
+    /// <summary>
+    /// child of DataSet
+    /// </summary>
+    [Serializable()]
+    public class Query
+    {
+        #region Serialization Interface
+        public string DataSourceName;
+        public List<Serialization.QueryParameter> QueryParameters = new List<Serialization.QueryParameter>();
+        public string CommandText;
+        #endregion
+    }
+}
