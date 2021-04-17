684d20326d9ded54bc34e48db87b6ae6f8155221
KhushbooBuddekar <khushi.buddekar@gmail.com>
2021-04-17 19:49:42 +0530

    commit

--- Areas/Configuration/Models/MatrixMasterModel.cs
+++ Areas/Configuration/Models/MatrixMasterModel.cs
@@ -12,7 +12,9 @@ namespace LIMS_DEMO.Areas.Configuration.Models
         public int MatrixId { get; set; }
         public int ProductGroupId { get; set; }
         public int SubGroupId { get; set; }
+        [Required(ErrorMessage = "Please Enter Matrix Name")]
         public string MatrixName { get; set; }
+        [Required(ErrorMessage = "Please Enter Matrix Code")]
         public string MatrixCode { get; set; }
         public string Description { get; set; }
         public bool IsActive { get; set; }
@@ -20,9 +22,12 @@ namespace LIMS_DEMO.Areas.Configuration.Models
         public System.DateTime EnteredDate { get; set; }
         public Nullable<int> ModifiedBy { get; set; }
         public byte[] ModifiedDate { get; set; }
+        [Required(ErrorMessage ="Please Select Sample Type Product Name")]
         public Nullable<int> SampleTypeProductId { get; set; }
         public string SampleTypeProductName { get; set; }
+        [Required(ErrorMessage = "Please Select Product Group Name")]
         public string ProductGroupName { get; set; }
+        [Required(ErrorMessage = "Please Select Sub Group Name")]
         public string SubGroupName { get; set; }
     }
 }
