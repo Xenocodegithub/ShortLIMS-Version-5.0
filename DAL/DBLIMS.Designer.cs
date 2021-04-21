afe1e0b533f1ef67ea3f3c772368b9e80da95c5a
guptanivedita92 <guptanivedita92@gmail.com>
2021-04-21 16:24:03 +0530

    Commit By Sandeep

--- DAL/DBLIMS.Designer.cs
+++ DAL/DBLIMS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\ShortLIMS\shortlims code\DAL\DBLIMS.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Sandeep\ShortLIms\Lims21April\DAL\DBLIMS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
